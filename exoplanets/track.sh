61c1fbaa101f62dd72e251b8b0bbd74fa6af3e28
EC2 Default User <ec2-user@ip-10-112-69-167.ec2.internal>
2012-06-07 01:10:46 +0000

    debugging

--- exoplanets/track.sh
+++ exoplanets/track.sh
@@ -8,7 +8,8 @@ LOGIN=`cat login`
 # Tracking
 for i in `seq 1 1000`;
 do
-        curl -silent --user $LOGIN --data "count=0&stall_warnings=true&track=$TRACK" https://stream.twitter.com/1/statuses/filter.json | /usr/sbin/rotatelogs data/%Y%m%d 86400
-        mail geert@barentsen.be -s "TweetTracker warning: $NAME was interrupted, i="$i < $0
+        curl --silent --user $LOGIN --data "count=0&stall_warnings=true&track=$TRACK" https://stream.twitter.com/1/statuses/filter.json | rotatelogs -f data/%Y%m%d 86400
+        #mail geert@barentsen.be -s "TweetTracker warning: $NAME was interrupted, i="$i < $0
+	echo "Interrupt i="$i
         sleep 60
 done
