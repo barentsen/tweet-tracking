48f93c0ce54a847bab1f76dcee4c1f0c40da4561
Geert Barentsen <geert@barentsen.be>
2012-06-08 00:04:23 +0100

    Ensured the tmux session is not opened twice

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+#
+# This script will start all the trackers in the background 
+# using a terminal multiplexer (tmux)
+#
+# "$tmux a -t twitter" to see the running output.
 
-tmux new-session -d -s twitter -n exoplanets
-tmux send-keys -t twitter:exoplanets "cd exoplanets; ./track.sh" Enter
 
-tmux new-window -t twitter -n spiders
-tmux send-keys -t twitter:spiders "cd spiders; ./track.sh" Enter
+# Count the number of tmux "twitter" windows already open
+RUNNING=`tmux ls 2> /dev/null | grep -c twitter`
+if [ $RUNNING -gt 0 ]; then
+    echo "ERROR: A tmux twitter session is already running! Not starting."
 
-#tmux new-window -t twitter -n meteors
+else
+
+    tmux new-session -d -s twitter -n meteors
+    tmux send-keys -t twitter:meteors "cd meteors; ./track.sh" Enter
+    
+    tmux new-window -t twitter -n exoplanets
+    tmux send-keys -t twitter:exoplanets "cd exoplanets; ./track.sh" Enter
+    
+    tmux new-window -t twitter -n spiders
+    tmux send-keys -t twitter:spiders "cd spiders; ./track.sh" Enter
+fi
+
