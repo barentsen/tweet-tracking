db14b3c762cbe49d4b7a0870ddf903fdc9ee6fbb
barentsen <geert@barentsen.be>
2014-11-26 10:53:26 +0000

    Moved the credentials to secrets.py

---
+++ exoplanets/track-exoplanets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""Sends JSON-formatted tweets related to exoplanets to stdout in real time
+
+Author: Geert Barentsen (geert.io)
+Inspired by: http://peter-hoffmann.com/2012/simple-twitter-streaming-api-access-with-python-and-oauth.html
+"""
+import sys
+import tweepy
+import secrets
+
+
+class CustomStreamListener(tweepy.StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+
+
+if __name__ == '__main__':
+    # Setup authentication
+    auth = tweepy.OAuthHandler(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET)
+    auth.set_access_token(secrets.ACCESS_KEY, secrets.ACCESS_SECRET)
+    # Start listening to the streaming API
+    sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+    sapi.filter(track=['exoplanet', 'exoplanets'], stall_warnings=True)
+
--- exoplanets/track.sh
+++ exoplanets/track.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-# Configuration
-#TRACK="exoplanet,exoplanets"
-#LOGIN=`cat login`
-
-# Tracking
 for i in `seq 1 100`;
 do
-        #curl --silent --user $LOGIN --data "count=0&stall_warnings=true&track=$TRACK" https://stream.twitter.com/1/statuses/filter.json | rotatelogs -f data/%Y%m%d 86400
         python track-exoplanets.py | rotatelogs -f data/%Y%m%d 86400
 	echo "Interrupt i="$i
         sleep 60
 done
+
